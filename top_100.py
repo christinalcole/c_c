38d18efd78fd5a8b2d61e1a2dbcb0299d1741d06
Christina L. Cole <christinalcole@gmail.com>
2018-02-14 00:33:30 -0500

    Persist Collection of Article Details
    For each URL in list, isolate article headline, author/source, post/edit time, and URL.  These details are added to a collection for subsequent use.
    
    Collection doesn't yet isolate actual text: consists of HTML elements instead.
    Collection is also only of URLs on single page, or about 12 items.

--- top_100.py
+++ top_100.py
@@ -12,6 +12,20 @@ soup = BeautifulSoup(page.text, 'html.parser')
 
 item = soup.select(".box-text-title > a")
 
-# isolate URL for each item
+# isolate and collect URLs for each item on page (~12 URLs)
+urls = []
 for a in item:
-    print("URL: ", a["href"])
+    urls.append(a["href"])
+
+#Iterate through collection of URLs, extract details for 12 articles
+articles = []
+for link in urls:
+    soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+#collect headline, author, time, URL
+    item2_headline = soup.select(".text-title")
+    item2_author_time = soup.select(".text-time")
+    item2_URL = link
+#persist collection
+    articles.append((item2_URL, item2_headline, item2_author_time))
+
+print(articles)
