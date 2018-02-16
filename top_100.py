b7f184ccee79a9eaeb7cbc9bc7c778a53e0d371f
Christina L. Cole <christinalcole@gmail.com>
2018-02-15 19:57:53 -0500

    fix while loop logic to limit URL collection to 16

--- top_100.py
+++ top_100.py
@@ -11,10 +11,10 @@ import csv
 from time import sleep
 from random import randint
 
-# site = "http://www.yangtse.com/app/internet/index.html"
-# page = requests.get(site)
-# soup = BeautifulSoup(page.text, 'html.parser')
-soup = BeautifulSoup(open("/Users/christinacole/Desktop/Technology - Yangtze Evening News Network.htm"), "html.parser")
+site = "http://www.yangtse.com/app/internet/index.html"
+page = requests.get(site)
+soup = BeautifulSoup(page.text, 'html.parser')
+# soup = BeautifulSoup(open("/Users/christinacole/Desktop/Technology - Yangtze Evening News Network.htm"), "html.parser")
 item = soup.select(".box-text-title > a")
 
 # isolate and collect URLs for each item on page (~12 URLs)
@@ -28,26 +28,27 @@ for a in item:
 print (len(urls))
 
 
-# while urls.count("http") < 101:
-#     n = 2
-#     j = 2
-#     page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
-#
-#
-#     # pause btwn 8-15s, monitor requests
-#     # sleep(randint(8,15))
-#     #
-#     # print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(urls.count("http")))
-#     n+=1
-#     j=+1
-#
-#     # isolate and collect URLS for each item on list_page
-#     soup = BeautifulSoup(page.text, 'html.parser')
-#     item = soup.select(".box-text-title > a")
-#
-#     for a in item:
-#         urls.append(a["href"])
-#
+while len(urls) < 16:
+    n = 2
+    j = 2
+    page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
+
+
+    # pause btwn 8-15s, monitor requests
+    # sleep(randint(8,15))
+
+    print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(urls.count("http")))
+    n+=1
+    j=+1
+
+    # isolate and collect URLS for each item on list_page
+    soup = BeautifulSoup(page.text, 'html.parser')
+    item = soup.select(".box-text-title > a")
+
+    for a in item:
+        while len(urls) < 16:
+            urls.append(a["href"])
+
 #
 #         #Iterate through collection of URLs, extract details for 12 articles
 #         articles = []
@@ -60,6 +61,8 @@ print (len(urls))
 #         #persist collection
 #             articles.append((item2_URL, item2_headline, item2_author_time))
 #
+print(urls)
+print (len(urls))
 # # print(articles)
 #
 # #Option for output to CSV file
