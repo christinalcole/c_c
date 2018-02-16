86eea83d8b33ae8d36be3dae7206c24cd5eb628a
Christina L. Cole <christinalcole@gmail.com>
2018-02-15 19:50:47 -0500

    Confirm List Holds 12 Items
    while loop has been commented out, because reading from static page
    that only has 12 links.  Initially, was having a problem with the
    loop's logic: instead of stopping at 15, 100, etc., loop kept going
    because urls.count("http") returned 0.  Switched back to using len

--- top_100.py
+++ top_100.py
@@ -23,43 +23,47 @@ for a in item:
     urls.append(a["href"])
 
 # print (urls)
+# print (urls.count("http"))
 
-while urls.count("http") < 101:
-    n = 2
-    j = 2
-    page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
+print (len(urls))
 
 
-    # pause btwn 8-15s, monitor requests
-    # sleep(randint(8,15))
-    #
-    # print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(urls.count("http")))
-    n+=1
-    j=+1
-
-    # isolate and collect URLS for each item on list_page
-    soup = BeautifulSoup(page.text, 'html.parser')
-    item = soup.select(".box-text-title > a")
-
-    for a in item:
-        urls.append(a["href"])
-
-
-        #Iterate through collection of URLs, extract details for 12 articles
-        articles = []
-        for link in urls:
-            soup = BeautifulSoup(requests.get(link).text, 'html.parser')
-        #collect headline, author, time, URL
-            item2_headline = soup.select(".text-title")
-            item2_author_time = soup.select(".text-time")
-            item2_URL = link
-        #persist collection
-            articles.append((item2_URL, item2_headline, item2_author_time))
-
-# print(articles)
-
-#Option for output to CSV file
-with open('articles.csv', 'a') as csv_file:
-    writer=csv.writer(csv_file)
-    for item2_URL, item2_headline, item2_author_time in articles:
-        writer.writerow([item2_URL, item2_headline, item2_author_time])
+# while urls.count("http") < 101:
+#     n = 2
+#     j = 2
+#     page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
+#
+#
+#     # pause btwn 8-15s, monitor requests
+#     # sleep(randint(8,15))
+#     #
+#     # print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(urls.count("http")))
+#     n+=1
+#     j=+1
+#
+#     # isolate and collect URLS for each item on list_page
+#     soup = BeautifulSoup(page.text, 'html.parser')
+#     item = soup.select(".box-text-title > a")
+#
+#     for a in item:
+#         urls.append(a["href"])
+#
+#
+#         #Iterate through collection of URLs, extract details for 12 articles
+#         articles = []
+#         for link in urls:
+#             soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+#         #collect headline, author, time, URL
+#             item2_headline = soup.select(".text-title")
+#             item2_author_time = soup.select(".text-time")
+#             item2_URL = link
+#         #persist collection
+#             articles.append((item2_URL, item2_headline, item2_author_time))
+#
+# # print(articles)
+#
+# #Option for output to CSV file
+# with open('articles.csv', 'a') as csv_file:
+#     writer=csv.writer(csv_file)
+#     for item2_URL, item2_headline, item2_author_time in articles:
+#         writer.writerow([item2_URL, item2_headline, item2_author_time])
