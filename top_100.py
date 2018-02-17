843952a1ffb4c835d14f168b4289ccc2cb6a303b
Christina L. Cole <christinalcole@gmail.com>
2018-02-16 19:58:28 -0500

    add ability to roughly check runtime, confirming that lxml is faster

--- top_100.py
+++ top_100.py
@@ -4,6 +4,7 @@
 # Alternate output: write to CSV file
 
 import code
+import time
 
 import requests
 from bs4 import BeautifulSoup
@@ -13,10 +14,12 @@ import csv
 from time import sleep
 from random import randint
 
-site = "http://www.yangtse.com/app/internet/index.html"
-page = requests.get(site)
-soup = BeautifulSoup(page.text, 'html.parser')
-# soup = BeautifulSoup(open("/Users/christinacole/Desktop/Technology - Yangtze Evening News Network.htm"), "lxml")
+start_time = time.time()
+
+# site = "http://www.yangtse.com/app/internet/index.html"
+# page = requests.get(site)
+# soup = BeautifulSoup(page.text, 'html.parser')
+soup = BeautifulSoup(open("/Users/christinacole/Desktop/Technology - Yangtze Evening News Network.htm"), "lxml")
 # soup.encoding = "GBK"
 # code.interact(local=dict(globals(), **locals()))
 item = soup.select(".box-text-title > a")
@@ -26,40 +29,43 @@ urls = []
 for a in item:
     urls.append(a["href"])
 
-while len(urls) < 100:
-    n = 2
-    j = 2
-    page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
+print(urls)
+print ("--- %s seconds ---" % (time.time() - start_time))
 
-
-    # pause btwn 8-15s, monitor requests
-    sleep(randint(8,15))
-
-    print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(len(urls)))
-    n+=1
-    j=+1
-
-    # isolate and collect URLS for each item on list_page
-    soup = BeautifulSoup(page.text, 'html.parser')
-    item = soup.select(".box-text-title > a")
-
-    #check this loop again--is it slow, or getting stuck?
-    for a in item:
-        while len(urls) < 100:
-            urls.append(a["href"])
-
-        #Iterate through collection of URLs, extract details for 12 articles
-        articles = []
-        for link in urls:
-            soup = BeautifulSoup(requests.get(link).text, 'html.parser')
-        #collect headline, author, time, URL
-            item2_headline = soup.select(".text-title")
-            item2_author_time = soup.select(".text-time")
-            item2_URL = link
-        #persist collection
-            articles.append((item2_URL, item2_headline, item2_author_time))
-
-print(articles)
+# while len(urls) < 100:
+#     n = 2
+#     j = 2
+#     page = requests.get("http://www.yangtse.com/app/internet/index_" + str(n) + ".html")
+#
+#
+#     # pause btwn 8-15s, monitor requests
+#     sleep(randint(8,15))
+#
+#     print("Request no."+str(j) + ", Index page "+str(n), "urls count is "+str(len(urls)))
+#     n+=1
+#     j=+1
+#
+#     # isolate and collect URLS for each item on list_page
+#     soup = BeautifulSoup(page.text, 'html.parser')
+#     item = soup.select(".box-text-title > a")
+#
+#     #check this loop again--is it slow, or getting stuck?
+#     for a in item:
+#         while len(urls) < 100:
+#             urls.append(a["href"])
+#
+#         #Iterate through collection of URLs, extract details for 12 articles
+#         articles = []
+#         for link in urls:
+#             soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+#         #collect headline, author, time, URL
+#             item2_headline = soup.select(".text-title")
+#             item2_author_time = soup.select(".text-time")
+#             item2_URL = link
+#         #persist collection
+#             articles.append((item2_URL, item2_headline, item2_author_time))
+#
+# print(articles)
 #
 # #Option for output to CSV file
 # with open('articles.csv', 'a') as csv_file:
